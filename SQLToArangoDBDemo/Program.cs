c65ab4ffe6da9e5b8beb87b2df35588e8ddab73e
Hadi Fadlallah <H.Fadlallah@hotmail.com>
2021-04-17 02:52:19 +0300

    improved code

--- SQLToArangoDBDemo/Program.cs
+++ SQLToArangoDBDemo/Program.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using SqlToArangoDB;
+﻿using SqlToArangoDB;
 namespace SQLToArangoDBDemo
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            using (SQLReader reader = new SQLReader("Server=DESKTOP-KCL006K\\DATASERVER;Database=GraphPL;Trusted_Connection=yes;"))
+            using (SQLReader reader = new SQLReader("Server=<machine name>\\<instance>;Database=<database name>;Trusted_Connection=yes;"))
             {
                 reader.GetNodes();
                 reader.GetEdges();
 
-                using (ArrangoDbWriter writer = new ArrangoDbWriter("http://localhost:8529", "TestGraph", "root", "123"))
+                using (ArrangoDbWriter writer = new ArrangoDbWriter("http://localhost:8529", "<database name>", "<user>", "<password>"))
                 {
                     writer.ImportNodes(reader.Nodes);
                     writer.ImportEdges(reader.Edges);
